a3ea72bd847bb42875cbdadf302d23f20a206184
jaydip015 <chavdajaydip015@gmail.com>
2024-03-28 19:16:03 +0530

    New Resource add and new dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,10 @@ dependencies {
 
     //for realtime database
     implementation("com.google.firebase:firebase-database")
+    //for camera
+    implementation ("androidx.camera:camera-core:1.3.0-rc01")
+    implementation ("androidx.camera:camera-camera2:1.3.0-rc01")
+    implementation ("androidx.camera:camera-lifecycle:1.3.0-rc01")
+    implementation ("androidx.camera:camera-view:1.3.0-rc01")
+    implementation ("androidx.camera:camera-extensions:1.3.0-rc01")
 }
