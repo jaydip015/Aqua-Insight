d0cc5ef5be697e38864eba4c85fbf776b163a6f9
jaydip015 <chavdajaydip015@gmail.com>
2024-03-28 19:22:24 +0530

    Permission Related To Camera Added here

--- app/src/main/java/com/example/aquainsight/NewRaiseActivty.java
+++ app/src/main/java/com/example/aquainsight/NewRaiseActivty.java
@@ -6,6 +6,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
@@ -18,23 +19,44 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 public class NewRaiseActivty extends AppCompatActivity {
+    private static final int REQUEST_CODE_PERMISSIONS = 10;
+    private static final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.CAMERA};
+
     TextView headline;
-    ImageButton select;
+    ImageButton select,opencmaera;
     ImageView prev;
-    Intent i;
+    Intent i,intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_raise_activty);
         i=getIntent();
+
+        //initialization
         headline=findViewById(R.id.headlineNR);
         select=findViewById(R.id.simage);
         prev=findViewById(R.id.prevIMG);
+        opencmaera=findViewById(R.id.ocamera);
+
+        //setting data and listener
         headline.setText(i.getStringExtra("Headline"));
+        opencmaera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent=new Intent(NewRaiseActivty.this, CameraActivty.class);
+                if(allPermissionsGranted()){
+                    startActivity(intent);
+                }else {
+                    ActivityCompat.requestPermissions(NewRaiseActivty.this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+                }
+
+            }
+        });
         select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,4 +82,23 @@ public class NewRaiseActivty extends AppCompatActivity {
 
                 }
             });
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
