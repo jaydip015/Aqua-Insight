f9ff08c3fb24d11de8a96deef24cf4c5bc415803
jaydip015 <chavdajaydip015@gmail.com>
2024-03-28 19:18:20 +0530

    file for opening Image into Bitmap

---
+++ app/src/main/java/com/example/aquainsight/ImageUtil.java
@@ -0,0 +1,33 @@
+package com.example.aquainsight;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class ImageUtil {
+    public static Bitmap decodeBitmapFromFile(String filePath, int reqWidth, int reqHeight) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            while ((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+}
