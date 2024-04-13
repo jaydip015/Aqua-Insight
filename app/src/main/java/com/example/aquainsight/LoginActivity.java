c9668ffe900e1160bf2805f16e61c8c9a8b0460e
jaydip015 <chavdajaydip015@gmail.com>
2024-04-13 17:41:11 +0530

    small change

--- app/src/main/java/com/example/aquainsight/LoginActivity.java
+++ app/src/main/java/com/example/aquainsight/LoginActivity.java
@@ -5,14 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.aquainsight.Files.progressbarAdapter;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
