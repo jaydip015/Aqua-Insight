1c93053dfa48de0e6560163471901c709b4f8398
jaydip015 <chavdajaydip015@gmail.com>
2024-04-28 14:16:39 +0530

    delete function working perfectly and second change is button text change according to imageview

--- app/src/main/java/com/example/aquainsight/Fragments/Adminbottomsheet.java
+++ app/src/main/java/com/example/aquainsight/Fragments/Adminbottomsheet.java
@@ -1,11 +1,15 @@
 package com.example.aquainsight.Fragments;
 
 import static com.example.aquainsight.NewRaiseActivty.ADD;
+import static com.example.aquainsight.NewRaiseActivty.DOCID;
+import static com.example.aquainsight.NewRaiseActivty.ICOLLECTION;
+import static com.example.aquainsight.NewRaiseActivty.ID;
 import static com.example.aquainsight.NewRaiseActivty.LAT;
 import static com.example.aquainsight.NewRaiseActivty.LINK;
 import static com.example.aquainsight.NewRaiseActivty.LONG;
+import static com.example.aquainsight.NewRaiseActivty.RISSUES;
+import static com.example.aquainsight.NewRaiseActivty.USERS;
 
-import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -13,20 +17,24 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
 import com.bumptech.glide.Glide;
-import com.example.aquainsight.NewRaiseActivty;
 import com.example.aquainsight.R;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -36,10 +44,15 @@ public class Adminbottomsheet extends BottomSheetDialogFragment {
     TextView headline,address,issue;
     String is;
     Map<String,Object> data;
+    String id;
+    FirebaseFirestore db;
+    Marker marker;
 
-    public Adminbottomsheet(Marker marker, Map<String,Object> map) {
+    public Adminbottomsheet(Marker marker, Map<String,Object> map, FirebaseFirestore db) {
         is= (String) marker.getTag();
         this.data=map;
+        this.db=db;
+        this.marker=marker;
     }
 
     @Override
@@ -53,6 +66,7 @@ public class Adminbottomsheet extends BottomSheetDialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.admin_bottom_sheet, container, false);
+        id=data.get(DOCID).toString();
         delete=view.findViewById(R.id.delete);
         showImage=view.findViewById(R.id.Show_image);
         headline=view.findViewById(R.id.headingA);
@@ -66,7 +80,21 @@ public class Adminbottomsheet extends BottomSheetDialogFragment {
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                db.collection(ICOLLECTION).document(id).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            db.collection(USERS).document(data.get(ID).toString()).update(RISSUES, FieldValue.arrayRemove(db.collection(ICOLLECTION).document(id)));
+                            marker.remove();
+                            dismiss();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
         showImage.setOnClickListener(new View.OnClickListener() {
@@ -77,8 +105,16 @@ public class Adminbottomsheet extends BottomSheetDialogFragment {
                 if(i==0){
                     Toast.makeText(getContext(), "Image not provided by reporting user", Toast.LENGTH_SHORT).show();
                 }else{
-                    img.setVisibility(View.VISIBLE);
-                    Glide.with(getContext()).load(data.get(LINK)).into(img);
+                    String  check=showImage.getText().toString();
+                    int c=check.compareTo("Show Image");
+                    if (c==0){
+                        showImage.setText("Hide Image");
+                        img.setVisibility(View.VISIBLE);
+                        Glide.with(getContext()).load(data.get(LINK)).into(img);
+                    }else {
+                        showImage.setText("Show Image");
+                        img.setVisibility(View.GONE);
+                    }
                 }
             }
         });
